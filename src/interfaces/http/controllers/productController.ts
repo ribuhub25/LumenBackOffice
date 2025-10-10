d94f8d121f910811a708a3df4edf0ce40eaea8ce
Jhonatan Riquelme <jhonatanribu25@gmail.com>
2025-10-10 16:24:22 -0500

    Se implemento metodo para guardar productos

--- src/interfaces/http/controllers/productController.ts
+++ src/interfaces/http/controllers/productController.ts
@@ -3,11 +3,14 @@ import { CreateProduct } from "../../../application/use-cases/Products/CreatePro
 import { ProductRepositoryImpl } from "../../../infrastructure/repositories/productRepositoryImpl";
 import { GetProducts } from "../../../application/use-cases/Products/GetProducts";
 import { GetProduct } from "../../../application/use-cases/Products/GetProduct";
+import { SaveProduct } from "../../../application/use-cases/Products/SaveProduct";
+import { Product } from "../../../domain/models/Product";
 
 const repository = new ProductRepositoryImpl();
 const cu_create_product = new CreateProduct(repository); 
 const cu_get_products = new GetProducts(repository); 
 const cu_get_product = new GetProduct(repository);
+const cu_save_product = new SaveProduct(repository);
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
@@ -38,3 +41,39 @@ export const getProduct = async (req: Request, res: Response) => {
   }
 };
 
+export const saveProduct = async (req: Request, res: Response) => {
+  try{
+    // if(!(isProduct(req.body))){
+    //   return res.status(400).json({ error: "Formato del Producto Invalido" });
+    // }
+    const result = await cu_save_product.execute(req.body);
+    res.status(200).json(result);
+  }catch(error){
+    console.error("❌ Error al guardar el producto:", error.message);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+}
+
+function isProduct(obj: any): obj is Product {
+  return (
+    typeof obj.id === 'number' &&
+    typeof obj.created_at === 'string' &&
+    typeof obj.name === 'string' &&
+    typeof obj.price === 'number' &&
+    typeof obj.rating === 'number' &&
+    typeof obj.review_count === 'number' &&
+    typeof obj.href === 'string' &&
+    typeof obj.description === 'string' &&
+    typeof obj.imageSrc === 'string' &&
+    typeof obj.imageAlt === 'string' &&
+    typeof obj.discount === 'number' &&
+    typeof obj.status === 'number' &&
+    typeof obj.brand_Id === 'number' &&
+    typeof obj.stock === 'number' &&
+    typeof obj.brand_name === 'string' &&
+    typeof obj.code === 'string' &&
+    typeof obj.final_price === 'number' &&
+    typeof obj.is_new === 'number' &&
+    typeof obj.categories === 'object'
+  );
+}
--- src/interfaces/http/routes/productRoutes.ts
+++ src/interfaces/http/routes/productRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+<<<<<<< HEAD
 import { createProduct, getProduct, getProducts } from "../controllers/productController";
 <<<<<<< HEAD
 import { getBrands } from "../controllers/brandController";
 =======
+=======
+import { createProduct, getProduct, getProducts, saveProduct } from "../controllers/productController";
+>>>>>>> 2134ccc (Se implemento metodo para guardar productos)
 import { getBrandOptions, getBrands } from "../controllers/brandController";
 import { getCategoryOptions } from "../controllers/categoryController";
 >>>>>>> 3efba90 (Se incluyo los metodos para listar los combos de marca y categorias)
@@ -17,5 +21,9 @@ router.get("/brands", getBrands);
 =======
 router.get("/brandlist", getBrandOptions);
 router.get("/categorylist", getCategoryOptions);
+<<<<<<< HEAD
 >>>>>>> 3efba90 (Se incluyo los metodos para listar los combos de marca y categorias)
+=======
+router.put("/product", saveProduct);
+>>>>>>> 2134ccc (Se implemento metodo para guardar productos)
 export default router;
