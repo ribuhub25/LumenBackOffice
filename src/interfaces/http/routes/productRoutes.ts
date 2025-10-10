2134ccc3a01b15f4f61fa5beacedafc7e249184c
Jhonatan Riquelme <jhonatanribu25@gmail.com>
2025-10-10 16:24:22 -0500

    Se implemento metodo para guardar productos

--- src/interfaces/http/routes/productRoutes.ts
+++ src/interfaces/http/routes/productRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createProduct, getProduct, getProducts } from "../controllers/productController";
+import { createProduct, getProduct, getProducts, saveProduct } from "../controllers/productController";
 import { getBrandOptions, getBrands } from "../controllers/brandController";
 import { getCategoryOptions } from "../controllers/categoryController";
 
@@ -10,4 +10,5 @@ router.get("/product/:id", getProduct);
 router.get("/brands", getBrands);
 router.get("/brandlist", getBrandOptions);
 router.get("/categorylist", getCategoryOptions);
+router.put("/product", saveProduct);
 export default router;
